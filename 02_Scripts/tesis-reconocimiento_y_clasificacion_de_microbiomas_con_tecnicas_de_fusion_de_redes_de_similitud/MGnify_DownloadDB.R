70626162da9d376620337cfe70ac2cee9092ec0d
WMAlex <karel-.-alex@hotmail.com>
2024-08-21 21:15:09 -0600

    Creacion de proyecto en R - Descarga de BD Scripts

---
+++ 02_Scripts/tesis-reconocimiento_y_clasificacion_de_microbiomas_con_tecnicas_de_fusion_de_redes_de_similitud/MGnify_DownloadDB.R
@@ -0,0 +1,48 @@
+###Descarga e instalacion de librerias## 
+install.packages(c("devtools","mia","plyr","dplyr", "reshape2","httr","urltools"))
+
+library(devtools)
+library(tidyverse)
+library(mia)
+library(plyr)
+library(dplyr)
+library(reshape2)
+library(httr)
+library(urltools)
+
+##########################################
+
+remotes::install_github("EBI-Metagenomics/MGnifyR")
+
+
+library(MGnifyR)
+library(phyloseq)
+# Set up the MGnify client instance
+mgclnt <- MgnifyClient(usecache = TRUE, cache_dir = '/tmp/MGnify_cache')
+
+# Retrieve the list of analyses associated with a study
+accession_list <- searchAnalysis(mgclnt, "studies", "MGYS00005058", usecache = TRUE)
+accession_list <- searchAnalysis(mgclnt, "studies", "MGYS00000800", usecache = TRUE)
+accession_list <- searchAnalysis(mgclnt, "studies", "MGYS00003121", usecache = TRUE)
+
+
+# Download all associated study/sample and analysis metadata
+meta_dataframe <- getMetadata(mgclnt, accession_list, usecache = TRUE)
+
+# Convert analyses outputs to a single `MultiAssayExperiment` object
+mae3 <- getResult(mgclnt, meta_dataframe$analysis_accession, usecache = TRUE)
+mae3
+
+library(mia)
+
+mae_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[1]]) 
+mae2_phyloseq<-makePhyloseqFromTreeSummarizedExperiment(mae2)
+mae3_phyloseq<-makePhyloseqFromTreeSummarizedExperiment(mae3[[1]])
+mae4_phyloseq<-makePhyloseqFromTreeSummarizedExperiment(mae3[[2]])
+mae5_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[2]]) 
+mae6_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[3]]) 
+mae7_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[4]]) 
+mae8_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[5]]) 
+
+View(sample_data(mae_phyloseq))
+  
