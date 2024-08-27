2bde0959a98aac708cf1e93cfa00ef9c4aa338b3
WMAlex <karel-.-alex@hotmail.com>
2024-08-27 06:09:24 -0600

    Update: Creacion de script para optimizacion en el proceso de datos en R utilizando Python - librerias de CUDA dan error

--- 02_Scripts/tesis-reconocimiento_y_clasificacion_de_microbiomas_con_tecnicas_de_fusion_de_redes_de_similitud/MGnify_DownloadDB.R
+++ 02_Scripts/tesis-reconocimiento_y_clasificacion_de_microbiomas_con_tecnicas_de_fusion_de_redes_de_similitud/MGnify_DownloadDB.R
@@ -9,7 +9,8 @@ library(dplyr)
 library(reshape2)
 library(httr)
 library(urltools)
-
+library(remotes)
+library(tensorflow)
 ##########################################
 
 remotes::install_github("EBI-Metagenomics/MGnifyR")
@@ -17,28 +18,27 @@ remotes::install_github("EBI-Metagenomics/MGnifyR")
 
 library(MGnifyR)
 library(phyloseq)
+library(assertive)
+
+
 # Set up the MGnify client instance
 mgclnt <- MgnifyClient(usecache = TRUE, cache_dir = '/tmp/MGnify_cache')
 
 # Retrieve the list of analyses associated with a study
-accession_list <- searchAnalysis(mgclnt, "studies", "MGYS00005058", usecache = TRUE)
-accession_list <- searchAnalysis(mgclnt, "studies", "MGYS00000800", usecache = TRUE)
-accession_list <- searchAnalysis(mgclnt, "studies", "MGYS00003121", usecache = TRUE)
-
+accession_list <- searchAnalysis(mgclnt, "studies", "MGYS00005592", usecache = TRUE)
 
 # Download all associated study/sample and analysis metadata
 meta_dataframe <- getMetadata(mgclnt, accession_list, usecache = TRUE)
 
 # Convert analyses outputs to a single `MultiAssayExperiment` object
-mae3 <- getResult(mgclnt, meta_dataframe$analysis_accession, usecache = TRUE)
-mae3
-
+mae <- getResult(mgclnt, meta_dataframe$analysis_accession, usecache = TRUE)
+mae
 library(mia)
 
 mae_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[1]]) 
-mae2_phyloseq<-makePhyloseqFromTreeSummarizedExperiment(mae2)
-mae3_phyloseq<-makePhyloseqFromTreeSummarizedExperiment(mae3[[1]])
-mae4_phyloseq<-makePhyloseqFromTreeSummarizedExperiment(mae3[[2]])
+mae2_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[2]])
+mae3_phyloseq<-makePhyloseqFromTreeSummarizedExperiment(mae[[1]])
+mae4_phyloseq<-makePhyloseqFromTreeSummarizedExperiment(mae[[2]])
 mae5_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[2]]) 
 mae6_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[3]]) 
 mae7_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[4]]) 
