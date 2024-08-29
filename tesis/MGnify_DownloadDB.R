ebe145e7c1d866f4a30d6194d416bd5c5d8bcd74
WMAlex <karel.vs.alex@gmail.com>
2024-08-29 13:47:32 -0600

    Update: Creacion de archivos rds y correcccion en procesamiento con GPU

--- tesis/MGnify_DownloadDB.R
+++ tesis/MGnify_DownloadDB.R
@@ -1,6 +1,18 @@
 ###Descarga e instalacion de librerias## 
-install.packages(c("devtools","mia","plyr","dplyr", "reshape2","httr","urltools"))
+#install.packages(c("devtools","mia","plyr","dplyr", "reshape2","httr","urltools","tidyverse"))
+## Instalación de Biomanager para descargar librerias externas al CRAN
 
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+## Versión actual funcional para r versión 4.4.1
+
+BiocManager::install(version = "3.19")
+
+## Descarga de MGnifyR con Biomanager
+BiocManager::install("MGnifyR")
+
+# Librerias necesarias para utilizar MGnifyR 
 library(devtools)
 library(tidyverse)
 library(mia)
@@ -10,29 +22,31 @@ library(reshape2)
 library(httr)
 library(urltools)
 library(remotes)
-library(tensorflow)
-##########################################
-
-remotes::install_github("EBI-Metagenomics/MGnifyR")
-
-
 library(MGnifyR)
 library(phyloseq)
 library(assertive)
 
+##########################################
+#Codigo recolectado del repositorio de GitHub de MGnify 
 
 # Set up the MGnify client instance
 mgclnt <- MgnifyClient(usecache = TRUE, cache_dir = '/tmp/MGnify_cache')
+
 
 # Retrieve the list of analyses associated with a study
 accession_list <- searchAnalysis(mgclnt, "studies", "MGYS00005592", usecache = TRUE)
 
 # Download all associated study/sample and analysis metadata
 meta_dataframe <- getMetadata(mgclnt, accession_list, usecache = TRUE)
+saveRDS(meta_dataframe,"metadata_MGYS00005592.rds")
 
 # Convert analyses outputs to a single `MultiAssayExperiment` object
+process_metadata <- function(mgclnt){
 mae <- getResult(mgclnt, meta_dataframe$analysis_accession, usecache = TRUE)
 mae
+}
+process_metadata()
+
 library(mia)
 
 mae_phyloseq <- makePhyloseqFromTreeSummarizedExperiment(mae[[1]]) 
